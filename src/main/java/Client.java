5127540665509a251713c37f8c401ff2f267c576
LayBrother <monk@list.ru>
2022-04-13 17:20:17 +0400

    Задача 8.1

---
+++ src/main/java/Client.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+        String host = "localhost";
+        int port = 8081;
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            out.println("GET / HTTP/1.1\n" + "Host: localhost(127.0.0.1)\n\n\n");
+            System.out.println("Сonnection to the server is completed.");
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+}
